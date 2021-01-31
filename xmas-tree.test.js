19e17ac3b3eeab830eb2080e9e709aa65000af85
Richard Ng <richardcrng@gmail.com>
2021-01-31 16:35:50 +0000

    Reorder tests into suggested sequence for TDD

--- xmas-tree.test.js
+++ xmas-tree.test.js
@@ -6,22 +6,14 @@ const {
   makeTreeTrunk,
 } = require("./xmas-tree");
 
-test("xmasTree returns an array modelling a Christmas tree of given foliage height", () => {
-  expect(xmasTree(5)).toEqual([
-    "____#____",
-    "___###___",
-    "__#####__",
-    "_#######_",
-    "#########",
-    "____#____",
-    "____#____",
-  ]);
-  expect(xmasTree(3)).toEqual(["__#__", "_###_", "#####", "__#__", "__#__"]);
-});
-
 test("findLineWidth finds the line width from a foliage height", () => {
   expect(findLineWidth(5)).toBe(9);
   expect(findLineWidth(3)).toBe(5);
+});
+
+test("makeTreeTrunk returns the tree trunk part of the Christmas tree with given foliage height", () => {
+  expect(makeTreeTrunk(5)).toEqual(["____#____", "____#____"]);
+  expect(makeTreeTrunk(3)).toEqual(["__#__", "__#__"]);
 });
 
 test("makeFoliageSegment returns the foliage segment for a given level of a tree of given foliage height", () => {
@@ -42,7 +34,15 @@ test("makeTreeFoliage returns the foliage for a tree of given foliage height", (
   expect(makeTreeFoliage(3)).toEqual(["__#__", "_###_", "#####"]);
 });
 
-test("makeTreeTrunk returns the tree trunk part of the Christmas tree with given foliage height", () => {
-  expect(makeTreeTrunk(5)).toEqual(["____#____", "____#____"]);
-  expect(makeTreeTrunk(3)).toEqual(["__#__", "__#__"]);
+test("xmasTree returns an array modelling a Christmas tree of given foliage height", () => {
+  expect(xmasTree(5)).toEqual([
+    "____#____",
+    "___###___",
+    "__#####__",
+    "_#######_",
+    "#########",
+    "____#____",
+    "____#____",
+  ]);
+  expect(xmasTree(3)).toEqual(["__#__", "_###_", "#####", "__#__", "__#__"]);
 });
